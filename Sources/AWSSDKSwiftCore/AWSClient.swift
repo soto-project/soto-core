f3fd123b8e53a4a55e6d6951c00a66e756ba4f1f
BinaryBeard <sam.bantner@me.com>
2017-07-07 10:39:11 -0400

    Adding Status Code to root level
    When dealing with JSON, the StatusCode object is not reported and returns nil in higher level APIs

--- Sources/AWSSDKSwiftCore/AWSClient.swift
+++ Sources/AWSSDKSwiftCore/AWSClient.swift
@@ -454,6 +454,7 @@ extension AWSClient {
         switch responseBody {
         case .json(let dictionary):
             outputDict = dictionary
+            outputDict["StatusCode"] = Int32(response.statusCode)
             
         case .xml(let node):
             let str = XMLNodeSerializer(node: node).serializeToJSON()

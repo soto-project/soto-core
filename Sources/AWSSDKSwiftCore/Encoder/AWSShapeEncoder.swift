d61e8a0b633578cc2ddfa432886624224fb838e5
Jonathan McAllister <jonathan@blast.com>
2018-05-11 15:59:17 -0700

    fix broken tests

--- Sources/AWSSDKSwiftCore/Encoder/AWSShapeEncoder.swift
+++ Sources/AWSSDKSwiftCore/Encoder/AWSShapeEncoder.swift
@@ -26,12 +26,12 @@ struct AWSShapeEncoder {
     func encodeToJSONUTF8Data<Input: AWSShape>(_ input: Input) throws -> Data {
         return try sharedJSONEncoder.encode(input)
     }
-    
+
     func encodeToXMLUTF8Data(_ input: AWSShape, attributes: XMLAttribute = [:]) throws -> Data? {
         let node = try encodeToXMLNode(input, attributes: attributes)
         return XMLNodeSerializer(node: node).serializeToXML().data(using: .utf8, allowLossyConversion: false)
     }
-    
+
     func encodeToXMLNode(_ input: AWSShape, attributes: XMLAttribute = [:]) throws -> XMLNode {
         let mirror = Mirror(reflecting: input)
         let name = "\(mirror.subjectType)"
@@ -39,12 +39,12 @@ struct AWSShapeEncoder {
         if let attr = attributes.filter({ $0.key == name }).first {
             xmlNode.attributes = attr.value
         }
-        
+
         for el in mirror.children {
             guard let label = el.label?.upperFirst() else {
                 continue
             }
-            
+
             guard let value = unwrap(any: el.value) else {
                 continue
             }
@@ -53,20 +53,20 @@ struct AWSShapeEncoder {
             case let v as AWSShape:
                 let cNode = try AWSShapeEncoder().encodeToXMLNode(v)
                 node.children.append(contentsOf: cNode.children)
-                
+
             case let v as [AWSShape]:
                 for vv in v {
                     let cNode = try AWSShapeEncoder().encodeToXMLNode(vv)
                     node.children.append(contentsOf: cNode.children)
                 }
-                
+
             default:
                 switch value {
                 case let v as [Any]:
                     for vv in v {
                         node.values.append("\(vv)")
                     }
-                    
+
                 case let v as [AnyHashable: Any]:
                     for (key, value) in v {
                         let cNode = XMLNode(elementName: "\(key)")
@@ -77,10 +77,10 @@ struct AWSShapeEncoder {
                     node.values.append("\(value)")
                 }
             }
-            
+
             xmlNode.children.append(node)
         }
-        
+
         return xmlNode
     }
 }

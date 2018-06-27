e702e3148d419a5f1b7296ec9818640034ff86e5
Joe Smith <yasumoto7@gmail.com>
2018-06-27 10:22:58 -0700

    Migrate package to Swift 4, remove support for Swift 3

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
-// swift-tools-version:3.1
-
+// swift-tools-version:4.1
 import PackageDescription
 
 let package = Package(
     name: "AWSSDKSwiftCore",
-    targets: [
-        Target(name: "AWSSDKSwiftCore")
+    products: [
+        .library(name: "AWSSDKSwiftCore", targets: ["AWSSDKSwiftCore"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/noppoMan/Prorsum.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/noppoMan/HypertextApplicationLanguage.git", majorVersion: 1)
+        .package(url: "https://github.com/noppoMan/Prorsum.git", .upToNextMajor(from: "0.3.3")),
+        .package(url: "https://github.com/Yasumoto/HypertextApplicationLanguage.git", .branch("master"))
+    ],
+    targets: [
+        .target(name: "AWSSDKSwiftCore", dependencies: ["Prorsum", "HypertextApplicationLanguage"]),
+        .testTarget(name: "AWSSDKSwiftCoreTests", dependencies: ["AWSSDKSwiftCore"])
     ]
 )

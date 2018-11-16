b6bffde7e7b540440b2b72fbcc8eff49391305bf
Jonathan McAllister <jonathan@blast.com>
2018-11-15 17:20:42 -0800

    update to latest versions of nio, and nio-ssl

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
         .library(name: "AWSSDKSwiftCore", targets: ["AWSSDKSwiftCore"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "1.8.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.11.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.3.2"),
         .package(url: "https://github.com/Yasumoto/HypertextApplicationLanguage.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-INIParser.git", .upToNextMajor(from: "3.0.0")),
     ],

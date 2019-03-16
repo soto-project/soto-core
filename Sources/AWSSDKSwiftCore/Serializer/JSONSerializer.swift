638a30adee24f32ebfccb06662cc64fe4b3727d2
Adam Fowler <adamfowler71@gmail.com>
2019-03-16 19:58:01 +0000

    serializeToFlatDictionary() returns correct json
    Format is Attributes.1.key=<key>&Attributes.1.value=<value> not
    Attributes.key=value

--- Sources/AWSSDKSwiftCore/Serializer/JSONSerializer.swift
+++ Sources/AWSSDKSwiftCore/Serializer/JSONSerializer.swift
@@ -11,27 +11,28 @@ struct JSONSerializer {
     func serializeToDictionary(_ data: Data) throws -> [String: Any] {
         return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] ?? [:]
     }
-    
+
     func serializeToFlatDictionary(_ data: Data) throws -> [String: Any] {
         func flatten(dictionary: [String: Any]) -> [String: Any] {
             var flatted: [String: Any] = [:]
-            
+
             func destructiveFlatten(dictionary: [String: Any]) {
                 for (key, value) in dictionary {
                     switch value {
                     case let value as [String: Any]:
-                        for (key2, value2) in flatten(dictionary: value) {
-                            switch value2 {
+                        for iterator in flatten(dictionary: value).enumerated() {
+                            switch iterator.element.value {
                             case let value2 as [String: Any]:
                                 destructiveFlatten(dictionary: value2)
-                                
+
                             case let values as [Any]: // TODO: values<Element> might be dictionary...
                                 for iterator in values.enumerated() {
                                     flatted["\(key).member.\(iterator.offset+1)"] = iterator.element
                                 }
-                                
+
                             default:
-                                flatted["\(key).\(key2)"] = value2
+                                flatted["\(key).entry.\(iterator.offset+1).key"] = iterator.element.key
+                                flatted["\(key).entry.\(iterator.offset+1).value"] = iterator.element.value
                             }
                         }
                         
@@ -39,18 +40,18 @@ struct JSONSerializer {
                         for iterator in values.enumerated() {
                             flatted["\(key).member.\(iterator.offset+1)"] = iterator.element
                         }
-                        
+
                     default:
                         flatted[key] = value
                     }
                 }
             }
-            
+
             destructiveFlatten(dictionary: dictionary)
-            
+
             return flatted
         }
-        
+
         return flatten(dictionary: try serializeToDictionary(data))
     }
 }

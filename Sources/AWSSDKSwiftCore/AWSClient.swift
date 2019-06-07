a843ce4e86ef697ac0db985defef738efd50bdd8
Adam Fowler <adamfowler71@gmail.com>
2019-06-07 16:15:03 +0100

    Decode json dictionary data values using .base64
    If dictionary is coming from json data then set the date decoding strategy to be base64

--- Sources/AWSSDKSwiftCore/AWSClient.swift
+++ Sources/AWSSDKSwiftCore/AWSClient.swift
@@ -480,6 +480,8 @@ extension AWSClient {
             payloadPath: Output.payloadPath,
             members: Output._members
         )
+        
+        let decoder = DictionaryDecoder()
 
         var responseHeaders: [String: String] = [:]
         for (key, value) in response.head.headers {
@@ -490,6 +492,7 @@ extension AWSClient {
         switch responseBody {
         case .json(let data):
             outputDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] ?? [:]
+            decoder.dataDecodingStrategy = .base64
 
         case .xml(let node):
             var outputNode = node
@@ -525,7 +528,7 @@ extension AWSClient {
             }
         }
 
-        return try DictionaryDecoder().decode(Output.self, from: outputDict)
+        return try decoder.decode(Output.self, from: outputDict)
     }
 
     private func validateCode(response: Response, members: [AWSShapeMember] = []) throws {

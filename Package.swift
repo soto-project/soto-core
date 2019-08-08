f3b8b01491dbe3d53524b30a2fd9ccb7c75f575f
Adam Fowler <adamfowler71@gmail.com>
2019-08-08 08:43:45 +0100

    Added CAWSSDKOpenSSL to shim over differences in OpenSSL versions
    Specifically how HMAC contexts are created

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.11.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.3.2"),
+        .package(url: "https://github.com/apple/swift-nio-ssl-support.git", from: "1.0.0"),
         .package(url: "https://github.com/Yasumoto/HypertextApplicationLanguage.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/PerfectlySoft/Perfect-INIParser.git", .upToNextMajor(from: "3.0.0")),
     ],
@@ -16,6 +17,7 @@ let package = Package(
         .target(
             name: "AWSSDKSwiftCore",
             dependencies: [
+                "CAWSSDKOpenSSL",
                 "HypertextApplicationLanguage",
                 "NIO",
                 "NIOHTTP1",
@@ -23,6 +25,7 @@ let package = Package(
                 "NIOFoundationCompat",
                 "INIParser"
             ]),
+        .target(name: "CAWSSDKOpenSSL"),
         .testTarget(name: "AWSSDKSwiftCoreTests", dependencies: ["AWSSDKSwiftCore"])
     ]
 )

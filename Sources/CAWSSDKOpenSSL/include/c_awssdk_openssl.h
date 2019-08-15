9cc688a2c2861d23863049cb643a6db2f349dd9b
Adam Fowler <adamfowler71@gmail.com>
2019-08-15 16:09:38 +0100

    Stop using CNIOBoringSSL
    NIO team say this is an internal module and should not be used. From @weissi "That we are vending BoringSSL is literally just an implementation detail and the API does change a lot."

--- Sources/CAWSSDKOpenSSL/include/c_awssdk_openssl.h
+++ Sources/CAWSSDKOpenSSL/include/c_awssdk_openssl.h
@@ -9,6 +9,8 @@
 #define C_AWSSDK_OPENSSL_H
 
 #include <openssl/hmac.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
 
 HMAC_CTX *AWSSDK_HMAC_CTX_new();
 void AWSSDK_HMAC_CTX_free(HMAC_CTX* ctx);

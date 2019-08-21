527071c3a67d1b19090bd33d603363611c38c809
Adam Fowler <adamfowler71@gmail.com>
2019-08-21 12:00:56 +0100

    Cleaned up output format

--- Sources/AWSSDKSwiftCore/Message/AWSMiddleware.swift
+++ Sources/AWSSDKSwiftCore/Message/AWSMiddleware.swift
@@ -28,34 +28,66 @@ public extension AWSServiceMiddleware {
 
 #if DEBUG
 
+/// Middleware class that outputs the contents of requests being sent to AWS and the bodies of the responses received
 public class AWSLoggingMiddleware : AWSServiceMiddleware {
-    init(logFunction : @escaping (String)->() = { print($0) }) {
-        self.logFunction = logFunction
+    
+    /// initialize AWSLoggingMiddleware class
+    /// - parameters:
+    ///     - log: Function to call with logging output
+    init(log : @escaping (String)->() = { print($0) }) {
+        self.log = log
     }
     
-    public func chain(request: AWSRequest) throws -> AWSRequest {
-        print("Request:")
-        print(request)
-        return request
-    }
-    public func chain(responseBody: Body) throws -> Body {
-        print("Response:")
-        switch responseBody {
+    func getBodyOutput(_ body: Body) -> String {
+        var output = ""
+        switch body {
         case .xml(let element):
-            print(element.description)
+            output += "\n"
+            output += element.description
         case .json(let data):
-            print(String(data: data, encoding: .utf8) ?? "Failed to convert JSON response to UTF8")
+            output += "\n"
+            output += String(data: data, encoding: .utf8) ?? "Failed to convert JSON response to UTF8"
         case .buffer(let data):
-            print("Data \(data.count) bytes")
+            output += "data (\(data.count) bytes)"
         case .text(let string):
-            print(string)
+            output += "\n\(string)"
+        case .empty:
+            output += "empty"
         default:
             break
         }
+        return output
+    }
+
+    func getHeadersOutput(_ headers: [String: Any?]) -> String {
+        if headers.count == 0 {
+            return "[]"
+        }
+        var output = "["
+        for header in headers {
+            output += "\n    \(header.key) : \(header.value ?? "nil")"
+        }
+        return output + "\n]"
+    }
+    
+    /// output request
+    public func chain(request: AWSRequest) throws -> AWSRequest {
+        log("Request: \(request.operation)")
+        log("\(request.httpMethod) \(request.url)")
+        log("Headers: " + getHeadersOutput(request.httpHeaders))
+        log("Body: " + getBodyOutput(request.body))
+        return request
+    }
+    
+    /// output response
+    public func chain(responseBody: Body) throws -> Body {
+        var output = "Response: "
+        output += getBodyOutput(responseBody)
+        log(output)
         return responseBody
     }
     
-    let logFunction : (String)->()
+    let log : (String)->()
 }
 
 #endif //DEBUG

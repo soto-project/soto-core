bae8f8478ffcfdef3c0606e2a8d3c396651093a5
Adam Fowler <adamfowler71@gmail.com>
2019-08-27 17:54:17 +0100

    Use CommonCrypto on mac
    This allows us to get rid of the dependency on libressl.
    We also don't need the AWSSDKOpenSSL Shim on mac anymore

--- Package.swift
+++ Package.swift
@@ -9,15 +9,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.11.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.3.2"),
-        .package(url: "https://github.com/apple/swift-nio-ssl-support.git", from: "1.0.0"),
-        .package(url: "https://github.com/Yasumoto/HypertextApplicationLanguage.git", .upToNextMajor(from: "1.1.0")),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-INIParser.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/swift-aws/HypertextApplicationLanguage.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/swift-aws/Perfect-INIParser.git", .upToNextMajor(from: "3.0.0")),
     ],
     targets: [
         .target(
             name: "AWSSDKSwiftCore",
             dependencies: [
-                "CAWSSDKOpenSSL",
                 "HypertextApplicationLanguage",
                 "NIO",
                 "NIOHTTP1",
@@ -25,7 +23,25 @@ let package = Package(
                 "NIOFoundationCompat",
                 "INIParser"
             ]),
-        .target(name: "CAWSSDKOpenSSL"),
         .testTarget(name: "AWSSDKSwiftCoreTests", dependencies: ["AWSSDKSwiftCore"])
     ]
 )
+
+// switch for whether to use CAWSSDKOpenSSL to shim between OpenSSL versions
+#if os(Linux)
+let useAWSSDKOpenSSLShim = true
+#else
+let useAWSSDKOpenSSLShim = false
+#endif
+
+// AWSSDKSwiftCore target
+let awsSdkSwiftCoreTarget = package.targets.first(where: {$0.name == "AWSSDKSwiftCore"})
+
+// Decide on where we get our SSL support from. Linux usses NIOSSL to provide SSL. Linux also needs CAWSSDKOpenSSL to shim across different OpenSSL versions for the HMAC functions.
+if useAWSSDKOpenSSLShim {
+    package.targets.append(.target(name: "CAWSSDKOpenSSL"))
+    awsSdkSwiftCoreTarget?.dependencies.append("CAWSSDKOpenSSL")
+    package.dependencies.append(.package(url: "https://github.com/apple/swift-nio-ssl-support.git", from: "1.0.0"))
+}
+
+

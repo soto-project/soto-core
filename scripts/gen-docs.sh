91d93604db4d52f75818ec0276d11499754948f8
Adam Fowler <adamfowler71@gmail.com>
2019-09-12 14:52:01 +0100

    Return to current branch not master after completing documentation build

--- scripts/gen-docs.sh
+++ scripts/gen-docs.sh
@@ -8,6 +8,8 @@ jazzy --clean
 # stash everything that isn't in docs
 git stash push -- ":(exclude)docs"
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
 git checkout gh-pages
 # copy contents of docs to docs/current replacing the ones that are already there
 rm -rf docs/current
@@ -19,5 +21,5 @@ git add --all docs
 git commit -m "Publish latest docs"
 git push
 # return to master branch
-git checkout master
+git checkout $current_branch
 git stash pop

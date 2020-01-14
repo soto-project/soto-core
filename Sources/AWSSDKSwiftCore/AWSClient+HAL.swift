6961e64b34a001a364657b5802827e327f698980
Adam Fowler <adamfowler71@gmail.com>
2020-01-14 18:17:48 +0100

    Replace hypertextApplicationLanguageProcess switch and if with a guard

--- Sources/AWSSDKSwiftCore/AWSClient+HAL.swift
+++ Sources/AWSSDKSwiftCore/AWSClient+HAL.swift
@@ -4,36 +4,34 @@ import Foundation
 extension AWSClient {
     /// process hal+json date. Extract properties from HAL
     func hypertextApplicationLanguageProcess(response: AWSResponse, members: [AWSShapeMember]) throws -> AWSResponse {
-        switch response.body {
-        case .json(let data):
-            if (response.headers["Content-Type"] as? String)?.contains("hal+json") == true {
-                let representation = try Representation.from(json: data)
-                var dictionary = representation.properties
-                for rel in representation.rels {
-                    guard let representations = representation.representations(for: rel) else {
-                        continue
-                    }
-
-                    // get member type hint
-                    guard let hint = members.filter({ $0.location?.name == rel }).first else {
-                        continue
-                    }
-
-                    switch hint.type {
-                    case .list:
-                        dictionary[rel] = representations.map({ $0.properties })
-
-                    default:
-                        dictionary[rel] = representations.map({ $0.properties }).first
-                    }
-                }
-                var response = response
-                response.body = .json(try JSONSerialization.data(withJSONObject: dictionary, options: []))
+        guard case .json(let data) = response.body,
+            let contentType = response.headers["Content-Type"] as? String,
+            contentType.contains("hal+json") else {
                 return response
-            }
-        default:
-            break
         }
+        
+        let representation = try Representation.from(json: data)
+        var dictionary = representation.properties
+        for rel in representation.rels {
+            guard let representations = representation.representations(for: rel) else {
+                continue
+            }
+
+            // get member type hint
+            guard let hint = members.filter({ $0.location?.name == rel }).first else {
+                continue
+            }
+
+            switch hint.type {
+            case .list:
+                dictionary[rel] = representations.map({ $0.properties })
+
+            default:
+                dictionary[rel] = representations.map({ $0.properties }).first
+            }
+        }
+        var response = response
+        response.body = .json(try JSONSerialization.data(withJSONObject: dictionary, options: []))
         return response
     }
 }

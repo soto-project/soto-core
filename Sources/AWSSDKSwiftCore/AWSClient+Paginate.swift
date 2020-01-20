c388045057d68cd1ea74b0624498a462a4655201
Adam Fowler <adamfowler71@gmail.com>
2020-01-20 12:32:20 +0000

    Rename variables

--- Sources/AWSSDKSwiftCore/AWSClient+Paginate.swift
+++ Sources/AWSSDKSwiftCore/AWSClient+Paginate.swift
@@ -21,17 +21,17 @@ extension AWSClient {
     /// - Parameters:
     ///   - input: Input for request
     ///   - command: Command to be paginated
-    ///   - contentsKey: The name of the objects to be paginated in the response object
+    ///   - resultKey: The name of the objects to be paginated in the response object
     ///   - tokenKey: The name of token in the response object to continue pagination
-    func paginate<Input: AWSPaginateable, Output: AWSShape, PaginateOutput: AWSShape>(input: Input, command: @escaping (Input)->EventLoopFuture<Output>, contentsKey: String, tokenKey: String) -> EventLoopFuture<[PaginateOutput]> {
+    func paginate<Input: AWSPaginateable, Output: AWSShape, PaginateOutput: AWSShape>(input: Input, command: @escaping (Input)->EventLoopFuture<Output>, resultKey: String, tokenKey: String) -> EventLoopFuture<[PaginateOutput]> {
         var list : [PaginateOutput] = []
         
         func paginatePart(input: Input) -> EventLoopFuture<[PaginateOutput]> {
             let objects = command(input).flatMap { (response: Output) -> EventLoopFuture<[PaginateOutput]> in
                 let mirror = Mirror(reflecting: response)
-                guard let contents = mirror.getAttribute(forKey: contentsKey) as? [PaginateOutput] else { return self.eventLoopGroup.next().makeSucceededFuture(list) }
+                guard let results = mirror.getAttribute(forKey: resultKey) as? [PaginateOutput] else { return self.eventLoopGroup.next().makeSucceededFuture(list) }
 
-                list.append(contentsOf: contents)
+                list.append(contentsOf: results)
 
                 guard let token = mirror.getAttribute(forKey: tokenKey) as? String else { return self.eventLoopGroup.next().makeSucceededFuture(list) }
 

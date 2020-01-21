b4c41deb887a8f24b06474ffa295035eabdfbd9f
Adam Fowler <adamfowler71@gmail.com>
2020-01-21 18:26:34 +0000

    Split paginate functions declarations across multiple lines

--- Sources/AWSSDKSwiftCore/AWSClient+Paginate.swift
+++ Sources/AWSSDKSwiftCore/AWSClient+Paginate.swift
@@ -8,11 +8,13 @@
 import NIO
 
 /// protocol for all AWSShapes that can be paginated.
-/// Adds an initialiser that does a copy but inserts a new pagination token
+/// Adds an initialiser that does a copy but inserts a new string based pagination token
 public protocol AWSPaginateStringToken: AWSShape {
     init(_ original: Self, token: String)
 }
 
+/// protocol for all AWSShapes that can be paginated.
+/// Adds an initialiser that does a copy but inserts a new integer based pagination token
 public protocol AWSPaginateIntToken: AWSShape {
     init(_ original: Self, token: Int)
 }
@@ -20,14 +22,21 @@ public protocol AWSPaginateIntToken: AWSShape {
 public extension AWSClient {
     /// If an AWS command is returning an arbituary sized array sometimes it adds support for paginating this array
     /// ie it will return the array in blocks of a defined size, each block also includes a token which can be used to access
-    /// the next block. This function returns a future that will contain the full contents of the array.
+    /// the next block. This function loads each block and calls a closure with each block as parameter.
     ///
     /// - Parameters:
     ///   - input: Input for request
     ///   - command: Command to be paginated
-    ///   - resultKey: The name of the objects to be paginated in the response object
+    ///   - resultKey: The keypath to the list of objects to be paginated
     ///   - tokenKey: The name of token in the response object to continue pagination
-    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result>(input: Input, command: @escaping (Input)->EventLoopFuture<Output>, resultKey: PartialKeyPath<Output>, tokenKey: KeyPath<Output, String?>, onPage: @escaping ([Result], EventLoop)->EventLoopFuture<Bool>) -> EventLoopFuture<Void> {
+    ///   - onPage: closure called with each block of entries
+    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result>(
+        input: Input,
+        command: @escaping (Input)->EventLoopFuture<Output>,
+        resultKey: PartialKeyPath<Output>,
+        tokenKey: KeyPath<Output, String?>,
+        onPage: @escaping ([Result], EventLoop)->EventLoopFuture<Bool>
+    ) -> EventLoopFuture<Void> {
         
         func paginatePart(input: Input) -> EventLoopFuture<Void> {
             let future = command(input).flatMap { (response: Output) -> EventLoopFuture<Void> in
@@ -49,17 +58,25 @@ public extension AWSClient {
 
     /// If an AWS command is returning an arbituary sized array sometimes it adds support for paginating this array
     /// ie it will return the array in blocks of a defined size, each block also includes a token which can be used to access
-    /// the next block. This function returns a future that will contain the full contents of the array.
+    /// the next block. This function loads each block and calls a closure with each block as parameter.
     ///
     /// This version returns two arrays
     ///
     /// - Parameters:
     ///   - input: Input for request
     ///   - command: Command to be paginated
-    ///   - resultKey1: The name of the first set of objects to be paginated in the response object
-    ///   - resultKey2: The name of the second set of objects to be paginated in the response object
+    ///   - resultKey1: The keypath to the first list of objects to be paginated
+    ///   - resultKey2: The keypath to the second list of objects to be paginated
     ///   - tokenKey: The name of token in the response object to continue pagination
-    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result1, Result2>(input: Input, command: @escaping (Input)->EventLoopFuture<Output>, resultKey1: PartialKeyPath<Output>, resultKey2: PartialKeyPath<Output>, tokenKey: KeyPath<Output, String?>, onPage: @escaping ([Result1], [Result2], EventLoop)->EventLoopFuture<Bool>) -> EventLoopFuture<Void> {
+    ///   - onPage: closure called with each block of entries
+    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result1, Result2>(
+        input: Input,
+        command: @escaping (Input)->EventLoopFuture<Output>,
+        resultKey1: PartialKeyPath<Output>,
+        resultKey2: PartialKeyPath<Output>,
+        tokenKey: KeyPath<Output, String?>,
+        onPage: @escaping ([Result1], [Result2], EventLoop)->EventLoopFuture<Bool>
+    ) -> EventLoopFuture<Void> {
 
         func paginatePart(input: Input) -> EventLoopFuture<Void> {
             let objects = command(input).flatMap { (response: Output) -> EventLoopFuture<Void> in
@@ -82,18 +99,27 @@ public extension AWSClient {
 
     /// If an AWS command is returning an arbituary sized array sometimes it adds support for paginating this array
     /// ie it will return the array in blocks of a defined size, each block also includes a token which can be used to access
-    /// the next block. This function returns a future that will contain the full contents of the array.
+    /// the next block. This function loads each block and calls a closure with each block as parameter.
     ///
     /// This version returns three arrays
     ///
     /// - Parameters:
     ///   - input: Input for request
     ///   - command: Command to be paginated
-    ///   - resultKey1: The name of the first set of objects to be paginated in the response object
-    ///   - resultKey2: The name of the second set of objects to be paginated in the response object
-    ///   - resultKey3: The name of the second set of objects to be paginated in the response object
+    ///   - resultKey1: The keypath to the first list of objects to be paginated
+    ///   - resultKey2: The keypath to the second list of objects to be paginated
+    ///   - resultKey3: The keypath to the third list of objects to be paginated
     ///   - tokenKey: The name of token in the response object to continue pagination
-    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result1, Result2, Result3>(input: Input, command: @escaping (Input)->EventLoopFuture<Output>, resultKey1: PartialKeyPath<Output>, resultKey2: PartialKeyPath<Output>, resultKey3: PartialKeyPath<Output>, tokenKey: KeyPath<Output, String?>, onPage: @escaping ([Result1], [Result2], [Result3], EventLoop)->EventLoopFuture<Bool>) -> EventLoopFuture<Void> {
+    ///   - onPage: closure called with each block of entries
+    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result1, Result2, Result3>(
+        input: Input,
+        command: @escaping (Input)->EventLoopFuture<Output>,
+        resultKey1: PartialKeyPath<Output>,
+        resultKey2: PartialKeyPath<Output>,
+        resultKey3: PartialKeyPath<Output>,
+        tokenKey: KeyPath<Output, String?>,
+        onPage: @escaping ([Result1], [Result2], [Result3], EventLoop)->EventLoopFuture<Bool>
+    ) -> EventLoopFuture<Void> {
 
         func paginatePart(input: Input) -> EventLoopFuture<Void> {
             let objects = command(input).flatMap { (response: Output) -> EventLoopFuture<Void> in
@@ -117,18 +143,29 @@ public extension AWSClient {
 
     /// If an AWS command is returning an arbituary sized array sometimes it adds support for paginating this array
     /// ie it will return the array in blocks of a defined size, each block also includes a token which can be used to access
-    /// the next block. This function returns a future that will contain the full contents of the array.
+    /// the next block. This function loads each block and calls a closure with each block as parameter.
     ///
     /// This version returns four arrays
     ///
     /// - Parameters:
     ///   - input: Input for request
     ///   - command: Command to be paginated
-    ///   - resultKey1: The name of the first set of objects to be paginated in the response object
-    ///   - resultKey2: The name of the second set of objects to be paginated in the response object
-    ///   - resultKey3: The name of the second set of objects to be paginated in the response object
+    ///   - resultKey1: The keypath to the first list of objects to be paginated
+    ///   - resultKey2: The keypath to the second list of objects to be paginated
+    ///   - resultKey3: The keypath to the third list of objects to be paginated
+    ///   - resultKey4: The keypath to the fourth list of objects to be paginated
     ///   - tokenKey: The name of token in the response object to continue pagination
-    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result1, Result2, Result3, Result4>(input: Input, command: @escaping (Input)->EventLoopFuture<Output>, resultKey1: PartialKeyPath<Output>, resultKey2: PartialKeyPath<Output>, resultKey3: PartialKeyPath<Output>, resultKey4: PartialKeyPath<Output>, tokenKey: KeyPath<Output, String?>, onPage: @escaping ([Result1], [Result2], [Result3], [Result4], EventLoop)->EventLoopFuture<Bool>) -> EventLoopFuture<Void> {
+    ///   - onPage: closure called with each block of entries
+    func paginate<Input: AWSPaginateStringToken, Output: AWSShape, Result1, Result2, Result3, Result4>(
+        input: Input,
+        command: @escaping (Input)->EventLoopFuture<Output>,
+        resultKey1: PartialKeyPath<Output>,
+        resultKey2: PartialKeyPath<Output>,
+        resultKey3: PartialKeyPath<Output>,
+        resultKey4: PartialKeyPath<Output>,
+        tokenKey: KeyPath<Output, String?>,
+        onPage: @escaping ([Result1], [Result2], [Result3], [Result4], EventLoop)->EventLoopFuture<Bool>
+    ) -> EventLoopFuture<Void> {
 
         func paginatePart(input: Input) -> EventLoopFuture<Void> {
             let objects = command(input).flatMap { (response: Output) -> EventLoopFuture<Void> in
@@ -153,16 +190,23 @@ public extension AWSClient {
 
     /// If an AWS command is returning an arbituary sized array sometimes it adds support for paginating this array
     /// ie it will return the array in blocks of a defined size, each block also includes a token which can be used to access
-    /// the next block. This function returns a future that will contain the full contents of the array.
+    /// the next block. This function loads each block and calls a closure with each block as parameter.
     ///
     /// This version uses an Int instead of a String for the token
     ///
     /// - Parameters:
     ///   - input: Input for request
     ///   - command: Command to be paginated
-    ///   - resultKey: The name of the objects to be paginated in the response object
+    ///   - resultKey: The keypath to the list of objects to be paginated
     ///   - tokenKey: The name of token in the response object to continue pagination
-    func paginate<Input: AWSPaginateIntToken, Output: AWSShape, Result>(input: Input, command: @escaping (Input)->EventLoopFuture<Output>, resultKey: PartialKeyPath<Output>, tokenKey: KeyPath<Output, Int?>, onPage: @escaping ([Result], EventLoop)->EventLoopFuture<Bool>) -> EventLoopFuture<Void> {
+    ///   - onPage: closure called with each block of entries
+    func paginate<Input: AWSPaginateIntToken, Output: AWSShape, Result>(
+        input: Input,
+        command: @escaping (Input)->EventLoopFuture<Output>,
+        resultKey: PartialKeyPath<Output>,
+        tokenKey: KeyPath<Output, Int?>,
+        onPage: @escaping ([Result], EventLoop)->EventLoopFuture<Bool>
+    ) -> EventLoopFuture<Void> {
         
         func paginatePart(input: Input) -> EventLoopFuture<Void> {
             let future = command(input).flatMap { (response: Output) -> EventLoopFuture<Void> in

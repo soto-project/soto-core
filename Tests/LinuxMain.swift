1770496c25b0fb6c3e866586cdc2a1cc6c18704c
Adam Fowler <adamfowler71@gmail.com>
2020-01-30 11:26:35 +0000

    Split JSONCoder and XMLCoder test into separate files

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -6,12 +6,13 @@ XCTMain([
     testCase(CredentialTests.allTests),
     testCase(DictionaryEncoderTests.allTests),
     testCase(HTTPClientTests.allTests),
+    testCase(JSONCoderTests.allTests),
     testCase(MetaDataServiceTests.allTests),
     testCase(PaginateTests.allTests),
     testCase(PerformanceTests.allTests),
-    testCase(SerializersTests.allTests),
     testCase(SignersV4Tests.allTests),
     testCase(TimeStampTests.allTests),
     testCase(ValidationTests.allTests),
+    testCase(XMLCoderTests.allTests),
     testCase(XMLTests.allTests)
 ])

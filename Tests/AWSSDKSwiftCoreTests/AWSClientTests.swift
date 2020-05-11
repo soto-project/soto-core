5176b0a1338a282f39cf9cd139aa928a3adc621d
Adam Fowler <adamfowler71@gmail.com>
2020-05-11 11:52:45 +0100

    Make Region and Partition structs so we can extend them

--- Tests/AWSSDKSwiftCoreTests/AWSClientTests.swift
+++ Tests/AWSSDKSwiftCoreTests/AWSClientTests.swift
@@ -1089,38 +1089,9 @@ class AWSClientTests: XCTestCase {
     }
 
     func testRegionEnum() {
-        let regions = [
-            "us-east-1",
-            "us-east-2",
-            "us-west-1",
-            "us-west-2",
-            "ap-south-1",
-            "ap-northeast-2",
-            "ap-southeast-1",
-            "ap-southeast-2",
-            "ap-northeast-1",
-            "ap-east-1",
-            "ca-central-1",
-            "eu-west-1",
-            "eu-west-3",
-            "eu-west-2",
-            "eu-central-1",
-            "eu-north-1",
-            "sa-east-1",
-            "me-south-1"
-        ]
-        regions.forEach {
-            let region = Region(rawValue: $0)
-            if case .other(_) = region {
-                XCTFail("\($0) is not a region")
-            }
-            let rawValue = region.rawValue
-            XCTAssertEqual(rawValue, $0)
-        }
-
         let region = Region(rawValue: "my-region")
-        if case .other(let regionName) = region {
-            XCTAssertEqual(regionName, "my-region")
+        if Region.other("my-region") == region {
+            XCTAssertEqual(region.rawValue, "my-region")
         } else {
             XCTFail("Did not construct Region.other()")
         }

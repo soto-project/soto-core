4f4acf2b1d3d4239d8d1fdc27c628badc464063d
Adam Fowler <adamfowler71@gmail.com>
2020-05-13 08:35:13 +0100

    Moved the key exists test into container and unkeyedContainer methods
    This allows us decode empty arrays

--- Tests/AWSSDKSwiftCoreTests/XMLCoderTests.swift
+++ Tests/AWSSDKSwiftCoreTests/XMLCoderTests.swift
@@ -234,6 +234,14 @@ class XMLCoderTests: XCTestCase {
         testDecodeEncode(type: Test.self, xml: xml)
     }
 
+    func testEmptyArrayDecodeEncode() {
+        struct Test : Codable {
+            let a : [Int]
+        }
+        let xml = "<Test></Test>"
+        testDecodeEncode(type: Test.self, xml: xml)
+    }
+
     func testArrayOfStructuresDecodeEncode() {
         struct Test2 : Codable {
             let b : String
@@ -250,14 +258,6 @@ class XMLCoderTests: XCTestCase {
             let a : [String:Int]
         }
         let xml = "<Test><a><first>1</first></a></Test>"
-        testDecodeEncode(type: Test.self, xml: xml)
-    }
-
-    func testDateDecodeEncode() {
-        struct Test : Codable {
-            let date : Date
-        }
-        let xml = "<Test><date>24876876234.5</date></Test>"
         testDecodeEncode(type: Test.self, xml: xml)
     }
 
@@ -487,7 +487,6 @@ class XMLCoderTests: XCTestCase {
             ("testArrayDecodeEncode", testArrayDecodeEncode),
             ("testArrayOfStructuresDecodeEncode", testArrayOfStructuresDecodeEncode),
             ("testDictionaryDecodeEncode", testDictionaryDecodeEncode),
-            ("testDateDecodeEncode", testDateDecodeEncode),
             ("testDataDecodeEncode", testDataDecodeEncode),
             ("testSerializeToXML", testSerializeToXML),
             ("testDecodeExpandedContainers", testDecodeExpandedContainers),

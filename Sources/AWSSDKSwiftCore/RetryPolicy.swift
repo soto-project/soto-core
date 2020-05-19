1d7d0668a4849b7caada5b98ebb171cc20743581
Adam Fowler <adamfowler71@gmail.com>
2020-05-19 12:30:00 +0100

    Enable JitterRetry RetryController by default (#238)
    - Default to JitterRetry()
    - RetryController.getRetryWaitTime returns RetryStatus enum
    - RetryController renamed to RetryPolicy

--- Sources/AWSSDKSwiftCore/RetryPolicy.swift
+++ Sources/AWSSDKSwiftCore/RetryPolicy.swift
@@ -15,49 +15,54 @@
 import NIO
 import NIOHTTP1
 
-/// Protocol for Retry controller. Returns amount of time before the next retry after an HTTP error
-public protocol RetryController {
+public enum RetryStatus {
+    case retry(wait: TimeAmount)
+    case dontRetry
+}
+
+/// Protocol for Retry strategy. Has function returning amount of time before the next retry after an HTTP error
+public protocol RetryPolicy {
     /// Returns whether we should retry (nil means don't) and how long we should wait before retrying
     /// - Parameters:
     ///   - error: Error returned by HTTP client
     ///   - attempt: retry attempt number
-    func getRetryWaitTime(error: Error, attempt: Int) -> TimeAmount?
+    func getRetryWaitTime(error: Error, attempt: Int) -> RetryStatus?
 }
 
 /// Retry controller that never returns a retry wait time
-public struct NoRetry: RetryController {
+public struct NoRetry: RetryPolicy {
     public init() {}
-    public func getRetryWaitTime(error: Error, attempt: Int) -> TimeAmount? {
-        return nil
+    public func getRetryWaitTime(error: Error, attempt: Int) -> RetryStatus? {
+        return .dontRetry
     }
 }
 
 /// Protocol for standard retry response. Will attempt to retry on 5xx errors, 429 (tooManyRequests).
-public protocol StandardRetryController: RetryController {
+public protocol StandardRetryStrategy: RetryPolicy {
     var maxRetries: Int { get }
     func calculateRetryWaitTime(attempt: Int) -> TimeAmount
 }
 
-public extension StandardRetryController {
+public extension StandardRetryStrategy {
     /// default version of getRetryWaitTime for StandardRetryController
-    func getRetryWaitTime(error: Error, attempt: Int) -> TimeAmount? {
-        guard attempt < maxRetries else { return nil }
+    func getRetryWaitTime(error: Error, attempt: Int) -> RetryStatus? {
+        guard attempt < maxRetries else { return .dontRetry }
         
         switch error {
         // server error or too many requests
         case AWSClient.InternalError.httpResponseError(let response):
             if (500...).contains(response.status.code) || response.status.code == 429 {
-                return calculateRetryWaitTime(attempt: attempt)
+                return .retry(wait: calculateRetryWaitTime(attempt: attempt))
             }
-            return nil
+            return .dontRetry
         default:
-            return nil
+            return .dontRetry
         }
     }
 }
 
 /// Retry with an exponentially increasing wait time between wait times
-public struct ExponentialRetry: StandardRetryController {
+public struct ExponentialRetry: StandardRetryStrategy {
     public let base: TimeAmount
     public let maxRetries: Int
     
@@ -76,7 +81,7 @@ public struct ExponentialRetry: StandardRetryController {
 /// Exponential jitter retry. Instead of returning an exponentially increasing retry time it returns a jittered version. In a heavy load situation
 /// where a large number of clients all hit the servers at the same time, jitter helps to smooth out the server response. See
 /// https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/ for details.
-public struct JitterRetry: StandardRetryController {
+public struct JitterRetry: StandardRetryStrategy {
     public let base: TimeAmount
     public let maxRetries: Int
     
--- Tests/AWSSDKSwiftCoreTests/TestUtils.swift
+++ Tests/AWSSDKSwiftCoreTests/TestUtils.swift
@@ -46,7 +46,7 @@ func createAWSClient(
     endpoint: String? = nil,
     serviceEndpoints: [String: String] = [:],
     partitionEndpoints: [Partition: (endpoint: String, region: Region)] = [:],
-    retryController: RetryController = NoRetry(),
+    retryPolicy: RetryPolicy = NoRetry(),
     middlewares: [AWSServiceMiddleware] = [],
     possibleErrorTypes: [AWSErrorType.Type] = [],
     httpClientProvider: AWSClient.HTTPClientProvider = .createNew
@@ -65,7 +65,7 @@ func createAWSClient(
         endpoint: endpoint,
         serviceEndpoints: serviceEndpoints,
         partitionEndpoints: partitionEndpoints,
-        retryController: retryController,
+        retryPolicy: retryPolicy,
         middlewares: middlewares,
         possibleErrorTypes: possibleErrorTypes,
         httpClientProvider: httpClientProvider

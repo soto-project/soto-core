bf222333a83ec973823a966b5f3b7070d313df68
Fabian Fett <fabianfett@mac.com>
2020-06-26 17:41:54 +0200

    Remove RotatingCredential

--- Tests/AWSSDKSwiftCoreTests/Credential/ExpiringCredentialTests.swift
+++ Tests/AWSSDKSwiftCoreTests/Credential/ExpiringCredentialTests.swift
@@ -17,35 +17,5 @@ import XCTest
 import NIO
 
 class ExpiringCredentialTests: XCTestCase {
-    func testRotatingCredential() {
-        let expiringIn: TimeInterval = 300
-        let accessKeyId = "abc123"
-        let secretAccessKey = "123abc"
-        let sessionToken = "xyz987"
-        let expiration = Date(timeIntervalSinceNow: expiringIn)
-        let cred = RotatingCredential(
-            accessKeyId: accessKeyId,
-            secretAccessKey: secretAccessKey,
-            sessionToken: sessionToken,
-            expiration: expiration)
-        
-        XCTAssertEqual(cred.accessKeyId, accessKeyId)
-        XCTAssertEqual(cred.secretAccessKey, secretAccessKey)
-        XCTAssertEqual(cred.sessionToken, sessionToken)
-        XCTAssertEqual(cred.expiration, expiration)
-        
-        XCTAssert(cred.isExpiring(within: expiringIn + 1))
-        XCTAssert(!cred.isExpiring(within: expiringIn - 1))
-        XCTAssert(!cred.isExpired)
-        
-        let neverExpiring = RotatingCredential(
-            accessKeyId: accessKeyId,
-            secretAccessKey: secretAccessKey,
-            sessionToken: sessionToken,
-            expiration: nil)
-        
-        XCTAssert(!neverExpiring.isExpiring(within: -5000))
-        XCTAssert(!neverExpiring.isExpired)
-    }
 }
 

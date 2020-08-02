dce57cf1f9735e7330c4359964f123e3e513c1bd
Adam Fowler <adamfowler71@gmail.com>
2020-08-02 13:11:20 +0100

    Remove AWSSignerV4 from docs

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -20,7 +20,6 @@ mkdir -p sourcekitten
 
 # generate source kitten json
 sourcekitten doc --spm-module "AWSSDKSwiftCore" > sourcekitten/AWSSDKSwiftCore.json;
-sourcekitten doc --spm-module "AWSSignerV4" > sourcekitten/AWSSignerV4.json;
 
 # generate documentation with jazzy
 jazzy --clean

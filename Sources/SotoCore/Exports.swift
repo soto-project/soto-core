f4a18d375e0b5408772aac08102afa617486f1fc
Adam Fowler <adamfowler71@gmail.com>
2020-09-23 10:58:32 +0100

    Remove BaggageContext, Logging exports

--- Sources/SotoCore/Exports.swift
+++ Sources/SotoCore/Exports.swift
@@ -15,10 +15,6 @@
 @_exported import protocol SotoSignerV4.Credential
 @_exported import struct SotoSignerV4.StaticCredential
 
-@_exported import protocol BaggageContext.Context
-
-@_exported import struct Logging.Logger
-
 @_exported import struct NIO.ByteBuffer
 @_exported import struct NIO.ByteBufferAllocator
 @_exported import protocol NIO.EventLoop

eef4dd7d2b55a95112d09effa65fa53120f4ba4f
Adam Fowler <adamfowler71@gmail.com>
2020-09-30 16:06:42 +0100

    Add ability to edit AWSService.config values
    AWSService.with(middlewares:timeout:byteBufferAllocator:options) returns a new version of the AWSService with edited attributes

---
+++ Tests/SotoCoreTests/AWSServiceTests.swift
@@ -0,0 +1,72 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Soto for AWS open source project
+//
+// Copyright (c) 2017-2020 the Soto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Soto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@testable import SotoCore
+import SotoTestUtils
+import XCTest
+
+class AWSServiceTests: XCTestCase {
+    struct TestService: AWSService {
+        var client: AWSClient
+        var config: AWSServiceConfig
+
+        /// init
+        init(client: AWSClient, config: AWSServiceConfig) {
+            self.client = client
+            self.config = config
+        }
+        /// patch init
+        init(from: Self, patch: AWSServiceConfig.Patch) {
+            client = from.client
+            config = from.config.with(patch: patch)
+        }
+
+    }
+
+    func testRegion() {
+        let client = createAWSClient()
+        defer { XCTAssertNoThrow(try client.syncShutdown()) }
+        let serviceConfig = createServiceConfig(region: .apnortheast2)
+        let service = TestService(client: client, config: serviceConfig)
+        XCTAssertEqual(service.region, .apnortheast2)
+    }
+
+    func testEndpoint() {
+        let client = createAWSClient()
+        defer { XCTAssertNoThrow(try client.syncShutdown()) }
+        let serviceConfig = createServiceConfig(endpoint: "https://my-endpoint.com")
+        let service = TestService(client: client, config: serviceConfig)
+        XCTAssertEqual(service.endpoint, "https://my-endpoint.com")
+    }
+
+    func testWith() {
+        let client = createAWSClient()
+        defer { XCTAssertNoThrow(try client.syncShutdown()) }
+        let serviceConfig = createServiceConfig()
+        let service = TestService(client: client, config: serviceConfig)
+        let service2 = service.with(timeout: .seconds(2048), options: .init(rawValue: 0x67ff))
+        XCTAssertEqual(service2.config.timeout, .seconds(2048))
+        XCTAssertEqual(service2.config.options, .init(rawValue: 0x67ff))
+    }
+
+    func testWithMiddleware() {
+        struct TestMiddleware: AWSServiceMiddleware { }
+        let client = createAWSClient()
+        defer { XCTAssertNoThrow(try client.syncShutdown()) }
+        let serviceConfig = createServiceConfig()
+        let service = TestService(client: client, config: serviceConfig)
+        let service2 = service.with(middlewares: [TestMiddleware()])
+        XCTAssertNotNil(service2.config.middlewares.first {type(of: $0) == TestMiddleware.self})
+    }
+}

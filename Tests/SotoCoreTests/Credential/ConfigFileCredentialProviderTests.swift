cb0b477f366987329afeec70d2097538b89161df
Adam Fowler <adamfowler71@gmail.com>
2020-10-12 14:39:52 +0100

    Added testAWSProfileConfigFile (#387)
    * Added testAWSProfileConfigFile
    * Update ConfigFileCredentialProviderTests.swift

--- Tests/SotoCoreTests/Credential/ConfigFileCredentialProviderTests.swift
+++ Tests/SotoCoreTests/Credential/ConfigFileCredentialProviderTests.swift
@@ -200,6 +200,35 @@ class ConfigFileCredentialProviderTests: XCTestCase {
         XCTAssertEqual(credential?.secretAccessKey, "AWSSECRETACCESSKEY")
     }
 
+    func testAWSProfileConfigFile() {
+        let credentials = """
+        [test-profile]
+        aws_access_key_id = TESTPROFILE-AWSACCESSKEYID
+        aws_secret_access_key = TESTPROFILE-AWSSECRETACCESSKEY
+        """
+        Environment.set("test-profile", for: "AWS_PROFILE")
+        defer { Environment.unset(name: "AWS_PROFILE") }
+
+        let filename = "credentials"
+        let filenameURL = URL(fileURLWithPath: filename)
+        XCTAssertNoThrow(try Data(credentials.utf8).write(to: filenameURL))
+        defer { XCTAssertNoThrow(try FileManager.default.removeItem(at: filenameURL)) }
+
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        let eventLoop = eventLoopGroup.next()
+        defer { XCTAssertNoThrow(try eventLoopGroup.syncShutdownGracefully()) }
+        let httpClient = HTTPClient(eventLoopGroupProvider: .shared(eventLoop))
+        defer { XCTAssertNoThrow(try httpClient.syncShutdown()) }
+        let factory = CredentialProviderFactory.configFile(credentialsFilePath: filenameURL.path)
+
+        let provider = factory.createProvider(context: .init(httpClient: httpClient, eventLoop: eventLoop, logger: TestEnvironment.logger))
+
+        var credential: Credential?
+        XCTAssertNoThrow(credential = try provider.getCredential(on: eventLoop, logger: TestEnvironment.logger).wait())
+        XCTAssertEqual(credential?.accessKeyId, "TESTPROFILE-AWSACCESSKEYID")
+        XCTAssertEqual(credential?.secretAccessKey, "TESTPROFILE-AWSSECRETACCESSKEY")
+    }
+
     func testConfigFileNotAvailable() {
         let filename = "credentials_not_existing"
         let filenameURL = URL(fileURLWithPath: filename)

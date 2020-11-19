cb5731e687ee87f95f9e4665756e6dbe0839db87
Adam Fowler <adamfowler71@gmail.com>
2020-11-19 15:21:57 +0000

    Add internal version of STS AssumeRole credential provider (#406)
    * Add internal version of STS AssumeRole
    
    Used by ConfigFile credential provider
    
    * swift format
    
    * Add Logger to test
    
    * Supply empty credential provider to stsAssumeRole provider
    
    Otherwise it errors saying it cannot find a credential provider
    
    * Added CredentialProviderSelector protocol
    
    This is a credential provider which has to select an internal credential provider. Use the protocol with the RuntimeSelectorCredentialProvider.
    
    * Setup ConfigFile credentials to work with internal provider
    
    AWSConfigFileCredentialProvider now conforms to CredentialProviderSelector. Loading the config file will select a credential provider to use, instead of supplying static credentials

--- Tests/SotoCoreTests/Credential/ConfigFileCredentialProviderTests.swift
+++ Tests/SotoCoreTests/Credential/ConfigFileCredentialProviderTests.swift
@@ -16,6 +16,7 @@ import AsyncHTTPClient
 import NIO
 @testable import SotoCore
 import SotoTestUtils
+import SotoXML
 import XCTest
 
 class ConfigFileCredentialProviderTests: XCTestCase {
@@ -150,7 +151,7 @@ class ConfigFileCredentialProviderTests: XCTestCase {
         XCTAssertEqual(unexpandedNewPath, unexpandableFilePath)
     }
 
-    func testConfigFileCredentialINIParser() {
+    func testConfigFileCredentialINIParser() throws {
         // setup
         let credentials = """
         [default]
@@ -166,12 +167,23 @@ class ConfigFileCredentialProviderTests: XCTestCase {
         defer { XCTAssertNoThrow(try eventLoopGroup.syncShutdownGracefully()) }
         let eventLoop = eventLoopGroup.next()
 //        let path = filenameURL.absoluteString
-        let future = AWSConfigFileCredentialProvider.fromSharedCredentials(credentialsFilePath: filenameURL.path, on: eventLoop)
+        let threadPool = NIOThreadPool(numberOfThreads: 1)
+        threadPool.start()
+        defer { XCTAssertNoThrow(try threadPool.syncShutdownGracefully()) }
+        let fileIO = NonBlockingFileIO(threadPool: threadPool)
 
-        var credential: StaticCredential?
+        let future = AWSConfigFileCredentialProvider.getSharedCredentialsFromDisk(
+            credentialsFilePath: filenameURL.path,
+            profile: "default",
+            on: eventLoop,
+            using: fileIO
+        )
+
+        var credential: CredentialProvider?
         XCTAssertNoThrow(credential = try future.wait())
-        XCTAssertEqual(credential?.accessKeyId, "AWSACCESSKEYID")
-        XCTAssertEqual(credential?.secretAccessKey, "AWSSECRETACCESSKEY")
+        let staticCredential = try XCTUnwrap(credential as? StaticCredential)
+        XCTAssertEqual(staticCredential.accessKeyId, "AWSACCESSKEYID")
+        XCTAssertEqual(staticCredential.secretAccessKey, "AWSSECRETACCESSKEY")
     }
 
     func testConfigFileSuccess() {
@@ -251,5 +263,87 @@ class ConfigFileCredentialProviderTests: XCTestCase {
     func testConfigFileShutdown() {
         let client = createAWSClient(credentialProvider: .configFile())
         XCTAssertNoThrow(try client.syncShutdown())
+    }
+
+    func testInternalSTSAssumeRoleProvider() throws {
+        let credentials = STSCredentials(
+            accessKeyId: "STSACCESSKEYID",
+            expiration: Date(timeIntervalSince1970: 87_387_346),
+            secretAccessKey: "STSSECRETACCESSKEY",
+            sessionToken: "STSSESSIONTOKEN"
+        )
+        let testServer = AWSTestServer(serviceProtocol: .xml)
+        defer { XCTAssertNoThrow(try testServer.stop()) }
+        let client = AWSClient(
+            credentialProvider: .internalSTSAssumeRole(
+                request: .init(roleArn: "arn:aws:iam::000000000000:role/test-sts-assume-role", roleSessionName: "testInternalSTSAssumeRoleProvider"),
+                credentialProvider: .empty,
+                region: .useast1,
+                endpoint: testServer.address
+            ),
+            httpClientProvider: .createNew,
+            logger: TestEnvironment.logger
+        )
+        defer { XCTAssertNoThrow(try client.syncShutdown()) }
+
+        XCTAssertNoThrow(try testServer.processRaw { _ in
+            let output = STSAssumeRoleResponse(credentials: credentials)
+            let xml = try XMLEncoder().encode(output)
+            let byteBuffer = ByteBufferAllocator().buffer(string: xml.xmlString)
+            let response = AWSTestServer.Response(httpStatus: .ok, headers: [:], body: byteBuffer)
+            return .result(response)
+        })
+        var result: Credential?
+        XCTAssertNoThrow(result = try client.credentialProvider.getCredential(on: client.eventLoopGroup.next(), logger: AWSClient.loggingDisabled).wait())
+        let stsCredentials = result as? STSCredentials
+        XCTAssertEqual(stsCredentials?.accessKeyId, credentials.accessKeyId)
+        XCTAssertEqual(stsCredentials?.expiration, credentials.expiration)
+        XCTAssertEqual(stsCredentials?.secretAccessKey, credentials.secretAccessKey)
+        XCTAssertEqual(stsCredentials?.sessionToken, credentials.sessionToken)
+    }
+}
+
+// Extend STSAssumeRoleRequest so it can be used with the AWSTestServer
+extension STSAssumeRoleRequest: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let roleArn = try container.decode(String.self, forKey: .roleArn)
+        let roleSessionName = try container.decode(String.self, forKey: .roleSessionName)
+        self.init(roleArn: roleArn, roleSessionName: roleSessionName)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case roleArn = "RoleArn"
+        case roleSessionName = "RoleSessionName"
+    }
+}
+
+// Extend STSAssumeRoleResponse so it can be used with the AWSTestServer
+extension STSAssumeRoleResponse: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(credentials, forKey: .credentials)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case credentials = "Credentials"
+    }
+}
+
+// Extend STSCredentials so it can be used with the AWSTestServer
+extension STSCredentials: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(accessKeyId, forKey: .accessKeyId)
+        try container.encode(expiration, forKey: .expiration)
+        try container.encode(secretAccessKey, forKey: .secretAccessKey)
+        try container.encode(sessionToken, forKey: .sessionToken)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case accessKeyId = "AccessKeyId"
+        case expiration = "Expiration"
+        case secretAccessKey = "SecretAccessKey"
+        case sessionToken = "SessionToken"
     }
 }

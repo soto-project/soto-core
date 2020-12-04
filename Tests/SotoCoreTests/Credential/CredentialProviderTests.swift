b790ed95c21096917abd801031c71a05b6f15df4
Adam Fowler <adamfowler71@gmail.com>
2020-12-04 17:56:28 +0000

    swift format

--- Tests/SotoCoreTests/Credential/CredentialProviderTests.swift
+++ Tests/SotoCoreTests/Credential/CredentialProviderTests.swift
@@ -101,7 +101,7 @@ class CredentialProviderTests: XCTestCase {
             XCTAssertEqual(error as? CredentialProviderError, .noProvider)
         }
     }
-    
+
     func testCredentialSelectorShutdown() {
         class TestCredentialProvider: CredentialProvider {
             var active = true
@@ -109,12 +109,12 @@ class CredentialProviderTests: XCTestCase {
             func getCredential(on eventLoop: EventLoop, logger: Logger) -> EventLoopFuture<Credential> {
                 return eventLoop.makeSucceededFuture(StaticCredential(accessKeyId: "", secretAccessKey: ""))
             }
-            
+
             func shutdown(on eventLoop: EventLoop) -> EventLoopFuture<Void> {
                 self.active = false
                 return eventLoop.makeSucceededFuture(())
             }
-            
+
             deinit {
                 XCTAssertEqual(active, false)
             }
@@ -134,7 +134,7 @@ class CredentialProviderTests: XCTestCase {
             }
         }
         let elg = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        defer { XCTAssertNoThrow(try elg.syncShutdownGracefully())}
+        defer { XCTAssertNoThrow(try elg.syncShutdownGracefully()) }
         let provider = CredentialProviderOwner(eventLoop: elg.next())
         XCTAssertNoThrow(try provider.shutdown(on: elg.next()).wait())
     }

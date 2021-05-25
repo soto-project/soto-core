dfebdfccfe11e2d93611e20ae272ab29e1c01f35
Adam Fowler <adamfowler71@gmail.com>
2021-05-25 10:03:30 +0100

    Added more waiter tests

--- Tests/SotoCoreTests/WaiterTests.swift
+++ Tests/SotoCoreTests/WaiterTests.swift
@@ -19,44 +19,104 @@ import SotoTestUtils
 import XCTest
 
 class WaiterTests: XCTestCase {
-    func testBasicWaiter() {
-        let awsServer = AWSTestServer(serviceProtocol: .json)
-        let config = createServiceConfig(serviceProtocol: .json(version: "1.1"), endpoint: awsServer.address)
-        let client = createAWSClient(credentialProvider: .empty, middlewares: [AWSLoggingMiddleware()])
-        defer {
-            XCTAssertNoThrow(try client.syncShutdown())
-            XCTAssertNoThrow(try awsServer.stop())
-        }
+    static var awsServer: AWSTestServer!
+    static var config: AWSServiceConfig!
+    static var client: AWSClient!
 
-        struct Input: AWSEncodableShape & Decodable {
-            let i: Int
-        }
-        struct Output: AWSDecodableShape & Encodable {
-            let i: Int
-        }
-        func operation(input: Input, logger: Logger, eventLoop: EventLoop?) -> EventLoopFuture<Output> {
-            client.execute(operation: "Basic", path: "/", httpMethod: .POST, serviceConfig: config, input: input, logger: logger, on: eventLoop)
-        }
+    class override func setUp() {
+        Self.awsServer = AWSTestServer(serviceProtocol: .json)
+        Self.config = createServiceConfig(serviceProtocol: .json(version: "1.1"), endpoint: awsServer.address)
+        Self.client = createAWSClient(credentialProvider: .empty, middlewares: [AWSLoggingMiddleware()])
+    }
+
+    class override func tearDown() {
+        XCTAssertNoThrow(try client.syncShutdown())
+        XCTAssertNoThrow(try awsServer.stop())
+    }
+
+    struct Input: AWSEncodableShape & Decodable {
+        let i: Int
+    }
+    struct Output: AWSDecodableShape & Encodable {
+        let i: Int
+    }
+    func operation(input: Input, logger: Logger, eventLoop: EventLoop?) -> EventLoopFuture<Output> {
+        Self.client.execute(operation: "Basic", path: "/", httpMethod: .POST, serviceConfig: Self.config, input: input, logger: logger, on: eventLoop)
+    }
+
+    func testBasicWaiter() {
         let waiter = AWSClient.Waiter(
-            acceptors: [.init(state: .success, matcher: AWSOutputMatcher(path: \Output.i, expected: 3))],
-            maxRetryAttempts: 20,
+            acceptors: [
+                .init(state: .success, matcher: AWSOutputMatcher(path: \Output.i, expected: 3))
+            ],
+            minDelayTime: .seconds(2),
+            maxDelayTime: .seconds(4),
             command: operation
         )
-        do {
-            let input = Input(i: 1)
-            let response = client.wait(input, waiter: waiter, maxWaitTime: .seconds(20), logger: TestEnvironment.logger)
+        let input = Input(i: 1)
+        let response = Self.client.wait(input, waiter: waiter, logger: TestEnvironment.logger)
 
-            var i = 0
-            try awsServer.process { (request: Input) -> AWSTestServer.Result<Output> in
-                //let receivedInput = try JSONDecoder().decode(Input.self, from: request.body)
-                i += 1
-                return .result(Output(i: i), continueProcessing: i < 3)
+        var i = 0
+        XCTAssertNoThrow(try Self.awsServer.process { (request: Input) -> AWSTestServer.Result<Output> in
+            i += 1
+            return .result(Output(i: i), continueProcessing: i < 3)
+        })
+
+        XCTAssertNoThrow(try response.wait())
+    }
+
+    func testTimeoutWaiter() {
+        let waiter = AWSClient.Waiter(
+            acceptors: [
+                .init(state: .success, matcher: AWSOutputMatcher(path: \Output.i, expected: 3))
+            ],
+            minDelayTime: .seconds(2),
+            maxDelayTime: .seconds(4),
+            command: operation
+        )
+        let input = Input(i: 1)
+        let response = Self.client.wait(input, waiter: waiter, maxWaitTime: .seconds(4), logger: TestEnvironment.logger)
+
+        var i = 0
+        XCTAssertNoThrow(try Self.awsServer.process { (request: Input) -> AWSTestServer.Result<Output> in
+            i += 1
+            return .result(Output(i: i), continueProcessing: i < 2)
+        })
+
+        XCTAssertThrowsError(try response.wait()) { error in
+            switch error {
+            case let error as AWSClient.ClientError where error == .waiterTimeout:
+                break
+            default:
+                XCTFail("\(error)")
             }
-
-            try response.wait()
-        } catch {
-            XCTFail("Unexpected error: \(error)")
         }
+    }
+
+    func testErrorWaiter() {
+        let waiter = AWSClient.Waiter(
+            acceptors: [
+                .init(state: .retry, matcher: AWSErrorMatcher(AWSClientError.accessDenied)),
+                .init(state: .success, matcher: AWSOutputMatcher(path: \Output.i, expected: 3))
+            ],
+            minDelayTime: .seconds(2),
+            maxDelayTime: .seconds(4),
+            command: operation
+        )
+        let input = Input(i: 1)
+        let response = Self.client.wait(input, waiter: waiter, logger: TestEnvironment.logger)
+
+        var i = 0
+        XCTAssertNoThrow(try Self.awsServer.process { (request: Input) -> AWSTestServer.Result<Output> in
+            i += 1
+            if i < 3 {
+                return .error(.accessDenied, continueProcessing: true)
+            } else {
+                return .result(Output(i: i), continueProcessing: false)
+            }
+        })
+
+        XCTAssertNoThrow(try response.wait())
     }
 
 }

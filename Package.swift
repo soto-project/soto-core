9743fe3dc43ef25d697481ae38f42e80b196e045
Adam Fowler <adamfowler71@gmail.com>
2022-01-01 16:41:03 +0000

    Update AHC version requirement to 1.7.0

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.2"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.0.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.3.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.7.0"),
         .package(url: "https://github.com/adam-fowler/jmespath.swift.git", from: "1.0.0"),
     ],
     targets: [

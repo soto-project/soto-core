a97d0fb578a3d181c6c829cd8d4588cb907d0971
Adam Fowler <adamfowler71@gmail.com>
2022-05-12 16:18:42 +0100

    File header

--- Sources/SotoCore/Utils/base64.swift
+++ Sources/SotoCore/Utils/base64.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Soto for AWS open source project
+//
+// Copyright (c) 2022 the Soto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Soto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 // This base64 implementation is heavily inspired by:
 
 // https://github.com/lemire/fastbase64/blob/master/src/chromiumbase64.c

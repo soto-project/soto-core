ba325bb142c28948065d6054ea96b61f5f38858c
Adam Fowler <adamfowler71@gmail.com>
2023-06-18 18:38:35 +0100

    Merge async files with parent implementations

--- Sources/SotoCore/AWSService.swift
+++ Sources/SotoCore/AWSService.swift
@@ -65,4 +65,42 @@ extension AWSService {
             options: options
         ))
     }
+
+    /// Generate a signed URL
+    /// - parameters:
+    ///     - url : URL to sign
+    ///     - httpMethod: HTTP method to use (.GET, .PUT, .PUSH etc)
+    ///     - headers: Headers that are to be used with this URL. Be sure to include these headers when you used the returned URL
+    ///     - expires: How long before the signed URL expires
+    ///     - logger: Logger to output to
+    /// - returns:
+    ///     A signed URL
+    public func signURL(
+        url: URL,
+        httpMethod: HTTPMethod,
+        headers: HTTPHeaders = HTTPHeaders(),
+        expires: TimeAmount,
+        logger: Logger = AWSClient.loggingDisabled
+    ) async throws -> URL {
+        return try await self.client.signURL(url: url, httpMethod: httpMethod, headers: headers, expires: expires, serviceConfig: self.config, logger: logger)
+    }
+
+    /// Generate signed headers
+    /// - parameters:
+    ///     - url : URL to sign
+    ///     - httpMethod: HTTP method to use (.GET, .PUT, .PUSH etc)
+    ///     - headers: Headers that are to be used with this URL. Be sure to include these headers when you used the returned URL
+    ///     - body: body payload to sign as well. While it is unnecessary to provide the body for S3 other services require it
+    ///     - logger: Logger to output to
+    /// - returns:
+    ///     A series of signed headers including the original headers provided to the function
+    public func signHeaders(
+        url: URL,
+        httpMethod: HTTPMethod,
+        headers: HTTPHeaders = HTTPHeaders(),
+        body: AWSPayload = .empty,
+        logger: Logger = AWSClient.loggingDisabled
+    ) async throws -> HTTPHeaders {
+        return try await self.client.signHeaders(url: url, httpMethod: httpMethod, headers: headers, body: body, serviceConfig: self.config, logger: logger)
+    }
 }
--- Sources/SotoCore/Waiters/AWSClient+Waiter+async.swift
+++ Sources/SotoCore/Waiters/AWSClient+Waiter+async.swift
@@ -14,72 +14,73 @@
 
 import Logging
 import NIOCore
+/*
+ extension AWSClient {
+     /// Returns when waiter polling returns a success state
+     /// or returns an error if the polling returns an error or timesout
+     ///
+     /// - Parameters:
+     ///   - input: Input parameters
+     ///   - waiter: Waiter to wait on
+     ///   - maxWaitTime: Maximum amount of time to wait
+     ///   - logger: Logger used to provide output
+     ///   - eventLoop: EventLoop to run API calls on
+     /// - Returns: EventLoopFuture that will be fulfilled once waiter has completed
+     public func waitUntil<Input, Output>(
+         _ input: Input,
+         waiter: Waiter<Input, Output>,
+         maxWaitTime: TimeAmount? = nil,
+         logger: Logger = AWSClient.loggingDisabled,
+         on eventLoop: EventLoop? = nil
+     ) async throws {
+         let maxWaitTime = maxWaitTime ?? waiter.maxDelayTime
+         let deadline: NIODeadline = .now() + maxWaitTime
 
-extension AWSClient {
-    /// Returns when waiter polling returns a success state
-    /// or returns an error if the polling returns an error or timesout
-    ///
-    /// - Parameters:
-    ///   - input: Input parameters
-    ///   - waiter: Waiter to wait on
-    ///   - maxWaitTime: Maximum amount of time to wait
-    ///   - logger: Logger used to provide output
-    ///   - eventLoop: EventLoop to run API calls on
-    /// - Returns: EventLoopFuture that will be fulfilled once waiter has completed
-    public func waitUntil<Input, Output>(
-        _ input: Input,
-        waiter: Waiter<Input, Output>,
-        maxWaitTime: TimeAmount? = nil,
-        logger: Logger = AWSClient.loggingDisabled,
-        on eventLoop: EventLoop? = nil
-    ) async throws {
-        let maxWaitTime = maxWaitTime ?? waiter.maxDelayTime
-        let deadline: NIODeadline = .now() + maxWaitTime
-
-        var attempt = 0
-        while true {
-            attempt += 1
-            let result: Result<Output, Error>
-            do {
-                result = try .success(await waiter.command(input, logger))
-            } catch {
-                result = .failure(error)
-            }
-            var acceptorState: WaiterState?
-            for acceptor in waiter.acceptors {
-                if acceptor.matcher.match(result: result.map { $0 }) {
-                    acceptorState = acceptor.state
-                    break
-                }
-            }
-            // if state has not been set then set it based on return of API call
-            let waiterState: WaiterState
-            if let state = acceptorState {
-                waiterState = state
-            } else if case .failure = result {
-                waiterState = .failure
-            } else {
-                waiterState = .retry
-            }
-            // based on state succeed, fail promise or retry
-            switch waiterState {
-            case .success:
-                return
-            case .failure:
-                if case .failure(let error) = result {
-                    throw error
-                } else {
-                    throw ClientError.waiterFailed
-                }
-            case .retry:
-                let wait = waiter.calculateRetryWaitTime(attempt: attempt, remainingTime: deadline - .now())
-                if wait < .seconds(0) {
-                    throw ClientError.waiterTimeout
-                } else {
-                    logger.trace("Wait \(wait.nanoseconds / 1_000_000)ms")
-                    try await Task.sleep(nanoseconds: UInt64(wait.nanoseconds))
-                }
-            }
-        }
-    }
-}
+         var attempt = 0
+         while true {
+             attempt += 1
+             let result: Result<Output, Error>
+             do {
+                 result = try .success(await waiter.command(input, logger))
+             } catch {
+                 result = .failure(error)
+             }
+             var acceptorState: WaiterState?
+             for acceptor in waiter.acceptors {
+                 if acceptor.matcher.match(result: result.map { $0 }) {
+                     acceptorState = acceptor.state
+                     break
+                 }
+             }
+             // if state has not been set then set it based on return of API call
+             let waiterState: WaiterState
+             if let state = acceptorState {
+                 waiterState = state
+             } else if case .failure = result {
+                 waiterState = .failure
+             } else {
+                 waiterState = .retry
+             }
+             // based on state succeed, fail promise or retry
+             switch waiterState {
+             case .success:
+                 return
+             case .failure:
+                 if case .failure(let error) = result {
+                     throw error
+                 } else {
+                     throw ClientError.waiterFailed
+                 }
+             case .retry:
+                 let wait = waiter.calculateRetryWaitTime(attempt: attempt, remainingTime: deadline - .now())
+                 if wait < .seconds(0) {
+                     throw ClientError.waiterTimeout
+                 } else {
+                     logger.trace("Wait \(wait.nanoseconds / 1_000_000)ms")
+                     try await Task.sleep(nanoseconds: UInt64(wait.nanoseconds))
+                 }
+             }
+         }
+     }
+ }
+ */

6531e319f8ce8f2e740b5505b095bacd8af4ab55
Adam Fowler <adamfowler71@gmail.com>
2023-06-20 16:04:01 +0100

    Remove EventLoops from CredentialProviders (#554)
    * Make credential providers async
    
    * Fix tests
    
    Also add support for initialising an ExpiringValue with an updating closure. Needed to add another state to get this working.
    
    * getTaskProviderTask -> getCredentialProviderTask
    
    * Format copyright 2023
    
    * validation changes
    
    * Fix issues with credential providers
    
    Add error state for ExpiringValue where previous getValue returned an error
    
    * Don't throw error during credential provider selection
    
    If credential provider fails to provide a credential don't throw an error when you are shutting it down
    
    * Shutdown test for rotating credential provider
    
    And fixed a bug
    
    * Added testDeferredCredentialProviderSetupShutdown

--- Sources/SotoCore/Credential/RuntimeSelectorCredentialProvider.swift
+++ Sources/SotoCore/Credential/RuntimeSelectorCredentialProvider.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Soto for AWS open source project
 //
-// Copyright (c) 2017-2022 the Soto project authors
+// Copyright (c) 2017-2023 the Soto project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -20,46 +20,40 @@ import SotoSignerV4
 /// get credentials from a list of possible credential providers. Goes through list of providers from start to end
 /// attempting to get credentials. Once it finds a `CredentialProvider` that supplies credentials use that
 /// one
-class RuntimeSelectorCredentialProvider: CredentialProviderSelector {
-    /// promise to find a credential provider
-    let startupPromise: EventLoopPromise<CredentialProvider>
-    let lock = NIOLock()
-    var _internalProvider: CredentialProvider?
+final class RuntimeSelectorCredentialProvider: CredentialProviderSelector {
+    private let getProviderTask: Task<CredentialProvider, Error>
 
     init(providers: [CredentialProviderFactory], context: CredentialProviderFactory.Context) {
-        self.startupPromise = context.eventLoop.makePromise(of: CredentialProvider.self)
-        self.startupPromise.futureResult.whenSuccess { result in
-            self.internalProvider = result
+        self.getProviderTask = Task {
+            try await Self.setupInternalProvider(providers: providers, context: context)
         }
-        self.setupInternalProvider(providers: providers, context: context)
+    }
+
+    func getCredentialProviderTask() async throws -> CredentialProvider {
+        try await self.getProviderTask.value
+    }
+
+    func cancelCredentialProviderTask() {
+        self.getProviderTask.cancel()
     }
 
     /// goes through list of providers. If provider is able to provide credentials then use that one, otherwise move onto the next
     /// provider in the list
-    private func setupInternalProvider(providers: [CredentialProviderFactory], context: CredentialProviderFactory.Context) {
-        func _setupInternalProvider(_ index: Int) {
-            guard index < providers.count else {
-                self.startupPromise.fail(CredentialProviderError.noProvider)
-                return
-            }
-            let providerFactory = providers[index]
+    private static func setupInternalProvider(
+        providers: [CredentialProviderFactory],
+        context: CredentialProviderFactory.Context
+    ) async throws -> CredentialProvider {
+        for providerFactory in providers {
             let provider = providerFactory.createProvider(context: context)
-            provider.getCredential(on: context.eventLoop, logger: context.logger).whenComplete { result in
-                switch result {
-                case .success:
-                    context.logger.debug("Select credential provider", metadata: ["aws-credential-provider": .string("\(provider)")])
-                    self.startupPromise.succeed(provider)
-                case .failure:
-                    context.logger.log(level: context.options.errorLogLevel, "Select credential provider failed")
-                    _setupInternalProvider(index + 1)
-                }
+            do {
+                _ = try await provider.getCredential(logger: context.logger)
+                context.logger.debug("Select credential provider", metadata: ["aws-credential-provider": .string("\(provider)")])
+                return provider
+            } catch {
+                try? await provider.shutdown()
+                context.logger.log(level: context.options.errorLogLevel, "Select credential provider failed", metadata: ["aws-credential-provider": .string("\(provider)")])
             }
         }
-
-        _setupInternalProvider(0)
+        return NullCredentialProvider()
     }
 }
-
-// can use @unchecked Sendable here as `_internalProvider`` is accessed via `internalProvider` which
-// protects access with a `NIOLock`
-extension RuntimeSelectorCredentialProvider: @unchecked Sendable {}

8159c05cc168327528199fdc464f72c2eb62b1c8
Adam Fowler <adamfowler71@gmail.com>
2023-07-06 23:15:47 +0100

    Fixes for when strict-concurrency is on (#563)

--- Sources/SotoCore/Concurrency/ExpiringValue.swift
+++ Sources/SotoCore/Concurrency/ExpiringValue.swift
@@ -21,7 +21,7 @@ import Logging
 /// value if it has expired or is about to expire. The type ensures there is only
 /// ever one value update running at any one time. If an update is already running
 /// when you call `getValue` it will wait on the current update function to finish.
-actor ExpiringValue<T> {
+actor ExpiringValue<T: Sendable> {
     enum State {
         /// No value is stored
         case noValue

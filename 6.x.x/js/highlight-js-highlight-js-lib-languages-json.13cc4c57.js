9574d1dee15228d8fdf4a7d420f2028a94ac948a
adam-fowler <adam-fowler@users.noreply.github.com>
2023-07-22 15:55:10 +0000

    Documentation for https://github.com/soto-project/soto-core/tree/1ddc6a9d21fa129e24886a0085e8b6091777f924
    Generated by gen-docs.yml

---
+++ 6.x.x/js/highlight-js-highlight-js-lib-languages-json.13cc4c57.js
@@ -0,0 +1,10 @@
+/*!
+ * This source file is part of the Swift.org open source project
+ *
+ * Copyright (c) 2021 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ *
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+ */
+"use strict";(self["webpackChunkswift_docc_render"]=self["webpackChunkswift_docc_render"]||[]).push([[4529],{4636:function(e,n,c){function t(e){const n={className:"attr",begin:/"(\\.|[^\\"\r\n])*"(?=\s*:)/,relevance:1.01},c={match:/[{}[\],:]/,className:"punctuation",relevance:0},t={beginKeywords:["true","false","null"].join(" ")};return{name:"JSON",contains:[n,c,e.QUOTE_STRING_MODE,t,e.C_NUMBER_MODE,e.C_LINE_COMMENT_MODE,e.C_BLOCK_COMMENT_MODE],illegal:"\\S"}}c.r(n),c.d(n,{default:function(){return t}})}}]);

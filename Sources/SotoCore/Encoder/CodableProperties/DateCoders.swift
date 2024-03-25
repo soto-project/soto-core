4e1a714d23b3c40648494e26193791ef9ddda1e2
Adam Fowler <adamfowler71@gmail.com>
2024-03-25 15:55:10 +0000

    Remove references to Swift 5.7

--- Sources/SotoCore/Encoder/CodableProperties/DateCoders.swift
+++ Sources/SotoCore/Encoder/CodableProperties/DateCoders.swift
@@ -22,10 +22,6 @@ import class Foundation.ISO8601DateFormatter
 import struct Foundation.Locale
 import struct Foundation.TimeZone
 
-#if compiler(<5.7)
-extension Date: @unchecked Sendable {}
-#endif
-
 // MARK: TimeStamp Coders
 
 /// Protocol for time stamp coders that use a DateFormatter. Use this to enforce the timestamp format we require, or to set the timestamp format output

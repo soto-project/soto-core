da626d5a58aea0f369bd9fef0ae6e923da756f4c
Adam Fowler <adamfowler71@gmail.com>
2026-04-03 22:54:28 +0100

    Fix formatting for swift scripts

--- scripts/generate-region-test.swift
+++ scripts/generate-region-test.swift
@@ -1,18 +1,4 @@
 #!/usr/bin/env swift sh
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Soto for AWS open source project
-//
-// Copyright (c) 2017-2020 the Soto project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE.txt for license information
-// See CONTRIBUTORS.txt for the list of Soto project authors
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-//===----------------------------------------------------------------------===//
-
 import Foundation
 import SotoCore  // soto-project/soto-core ~> 5.0.0-beta.3.0
 import SotoSSM  // soto-project/soto ~> 5.0.0-beta.3.0

1d9d140e15720df87b26403b5cd2b6d451f2dece
Adam Fowler <adamfowler71@gmail.com>
2026-04-03 23:00:29 +0100

    Rename files to ignore swift formatting

--- scripts/generate-errors.sh
+++ scripts/generate-errors.sh
@@ -1,4 +1,18 @@
-#!/usr/bin/env swift sh  
+#!/usr/bin/env swift sh    
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Soto for AWS open source project
+//
+// Copyright (c) 2017-2020 the Soto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Soto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import Foundation
 import Stencil  // soto-project/Stencil
 

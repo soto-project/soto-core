7436d55031bd98fd46bac9e53ef33f8d45d9188f
Adam Fowler <adamfowler71@gmail.com>
2026-09-19 15:17:58 +0100

    Require swift-service-lifecycle 2.12.0

--- Package.swift
+++ Package.swift
@@ -53,7 +53,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.13.1"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.30.0"),
         .package(url: "https://github.com/jmespath/jmespath.swift.git", from: "1.0.2"),
-        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.8.0"),
+        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.12.0"),
     ],
     targets: [
         .target(

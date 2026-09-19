b3bc3d402877bbeca260eaa2df3790f5ee19ab94
Adam Fowler <adamfowler71@gmail.com>
2026-09-19 15:05:34 +0100

    Update for swift 6.4
    Drop swift 6.1
    Add swift 6.4 CI

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.1
+// swift-tools-version:6.2
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the Soto for AWS open source project
